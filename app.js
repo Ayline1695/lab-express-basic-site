4b479a52c8a22dd8fd07162532b7dca48e51d9a0
ayline <ayline1695@hotmail.com>
2020-11-24 12:13:31 +0100

    styles

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+const express = require('express');
+const app = express();
+app.use(express.static(`${__dirname}/views`))
+
+app.get('/',(req, res, next) => res.sendFile(__dirname + '/views/index.html'));
+
+app.get('/works',(req, res, next) => res.sendFile(__dirname + '/views/works.html'));
+
+app.get('/gallery',(req, res, next) => res.sendFile(__dirname + '/views/gallery.html'));
+
+app.get('/about',(req, res, next) => res.sendFile(__dirname + '/views/about.html'));
+
+
+
+app.listen(4000, () => console.log("port 4000"));
